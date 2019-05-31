14263d75f01e69e8640531ff5ff048b1279ddb6a
UKiyanitsa <u.kiyanitsa@gmail.com>
2019-05-31 11:14:57 +0300

    first

--- src/App/DoubleExpression.java
+++ src/App/DoubleExpression.java
@@ -1,7 +1,7 @@
 package App;
 
 public class DoubleExpression {
-    public static boolean doubleExpression(double a, double b, double c) {
+    public final static boolean doubleExpression(double a, double b, double c) {
         final double precision = 1e-4;
         return (Math.abs((a + b) - c) < precision);
     }
