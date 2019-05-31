dda3c2a8c074ded37414200cb1d02f3372616b3d
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-31 23:22:37 +0300

    Update DoubleExpression.java

--- src/App/DoubleExpression.java
+++ src/App/DoubleExpression.java
@@ -1,8 +1,8 @@
 package App;
 
 public class DoubleExpression {
-    public final static boolean doubleExpression(double firstNumber, double secondNumber, double comparisonТгьиук) {
+    public final static boolean doubleExpression(double firstNumber, double secondNumber, double comparisonNumber) {
         final double PRECISION = 1e-4;
-        return (Math.abs((firstNumber + secondNumber) - comparisonТгьиук) < PRECISION);
+        return (Math.abs((firstNumber + secondNumber) - comparisonNumber) < PRECISION);
     }
 }
